f0029ca0fb33217cde04626304c17a8ad01d28f4
LAPTOP-HCI5LH3L\suvit <suvi.t.hakala@helsinki.fi>
2021-03-30 06:39:46 +0300

    viikko2 javaa

--- laskarit/viikko2/src/test/MaksukorttiTest.java
+++ laskarit/viikko2/src/test/MaksukorttiTest.java
@@ -7,37 +7,47 @@ import org.junit.Test;
 
 public class MaksukorttiTest {
 
-    public MaksukorttiTest() {
-    }
-
-    @BeforeClass
-    public static void setUpClass() {
-    }
-
-    @AfterClass
-    public static void tearDownClass() {
-    }
+    Maksukortti kortti;
 
     @Before
     public void setUp() {
+        kortti = new Maksukortti(10);
     }
-
-    @After
-    public void tearDown() {
-    }
-
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    @Test
-    public void hello() {}
 
     @Test
     public void konstruktoriAsettaaSaldonOikein() {
-        Maksukortti kortti = new Maksukortti(10);
+        assertEquals("Kortilla on rahaa 10.0 euroa", kortti.toString());
+    }
 
-        String vastaus = kortti.toString();
+    @Test
+    public void syoEdullisestiVahentaaSaldoaOikein() {
+        kortti.syoEdullisesti();
+        assertEquals("Kortilla on rahaa 7.5 euroa", kortti.toString());
+    }
 
-        assertEquals("Kortilla on rahaa 7.0 euroa", vastaus);
+    @Test
+    public void syoMaukkaastiVahentaaSaldoaOikein() {
+        kortti.syoMaukkaasti();
+        assertEquals("Kortilla on rahaa 6.0 euroa", kortti.toString());
+    }
+
+    @Test
+    public void syoEdullisestiEiVieSaldoaNegatiiviseksi() {
+        kortti.syoMaukkaasti();
+        kortti.syoMaukkaasti();
+        kortti.syoEdullisesti();
+        assertEquals("Kortilla on rahaa 2.0 euroa", kortti.toString());
+    }
+
+    @Test
+    public void kortilleVoiLadataRahaa() {
+        kortti.lataaRahaa(25);
+        assertEquals("Kortilla on rahaa 35.0 euroa", kortti.toString());
+    }
+
+    @Test
+    public void kortinSaldoEiYlitaMaksimiarvoa() {
+        kortti.lataaRahaa(200);
+        assertEquals("Kortilla on rahaa 150.0 euroa", kortti.toString());
     }
 }
